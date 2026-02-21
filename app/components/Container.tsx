dfb4c57554d19d770c0df0f4b56d5ac3dd3e9c49
hazichomwarr <batchiy6@gmail.com>
2026-02-21 16:01:39 -0500

    feat: Add page transition wrapper to the container wrapper

--- app/components/Container.tsx
+++ app/components/Container.tsx
@@ -1,6 +1,11 @@
 // components/Container.tsx
 import React from "react";
+import PageTransition from "./ui/PageTransition";
 
 export default function Container({ children }: { children: React.ReactNode }) {
-  return <div className="mx-auto w-full max-w-6xl px-4">{children}</div>;
+  return (
+    <PageTransition>
+      <div className="mx-auto w-full max-w-6xl px-4">{children}</div>
+    </PageTransition>
+  );
 }
