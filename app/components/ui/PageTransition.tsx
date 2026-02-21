c4c06019d44faba7fb7bf90c8035481bac12e181
hazichomwarr <batchiy6@gmail.com>
2026-02-21 17:29:33 -0500

    refactor(UI): change transition to fade-in

--- app/components/ui/PageTransition.tsx
+++ app/components/ui/PageTransition.tsx
@@ -6,11 +6,17 @@ import { ReactNode } from "react";
 
 export default function PageTransition({ children }: { children: ReactNode }) {
   return (
+    // <motion.div
+    //   initial={{ opacity: 0, y: 10 }}
+    //   animate={{ opacity: 1, y: 0 }}
+    //   exit={{ opacity: 0, y: -10 }}
+    //   transition={{ duration: 0.5, ease: "easeOut" }}
+    // >
     <motion.div
-      initial={{ opacity: 0, y: 10 }}
-      animate={{ opacity: 1, y: 0 }}
-      exit={{ opacity: 0, y: -10 }}
-      transition={{ duration: 0.25, ease: "easeOut" }}
+      initial={{ opacity: 0, scale: 0.985 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.985 }}
+      transition={{ duration: 0.6, delay: 0.15, ease: "easeOut" }}
     >
       {children}
     </motion.div>
