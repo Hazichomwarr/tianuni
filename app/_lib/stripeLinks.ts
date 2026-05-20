a3f0f1283540b8d4c8361f042264ad643de5a3c2
hazichomwarr <batchiy6@gmail.com>
2026-05-20 15:41:42 -0400

    feat: add new stipe links for the payments

--- app/_lib/stripeLinks.ts
+++ app/_lib/stripeLinks.ts
@@ -1,7 +1,7 @@
 // app/_lib/stripeLinks.ts
 export const STRIPE_LINKS = {
-  adhesion: "https://buy.stripe.com/test_14AbJ2geJ1kddzt1gp3VC00",
+  adhesion: "https://buy.stripe.com/test_28E14ogeJgf73YT5wF3VC05",
   donation: "https://buy.stripe.com/test_aFaaEYbYtd2Vanh1gp3VC02",
-  eventAdult: "https://buy.stripe.com/test_8x28wQ0fL0g9brle3b3VC01",
-  eventChild: "https://buy.stripe.com/test_7sY00k3rXaUNanh7EN3VC03",
+  eventAdult: "https://buy.stripe.com/test_8x26oI5A5gf72UP9MV3VC06",
+  eventChild: "https://buy.stripe.com/test_cNi14obYt5At6710cl3VC04",
 } as const;
