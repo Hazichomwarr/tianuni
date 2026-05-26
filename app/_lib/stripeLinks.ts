6ec9ad53100519b906598b24475ab39cc1d34184
hazichomwarr <batchiy6@gmail.com>
2026-05-26 12:51:38 -0400

    feat: switch test payment links to live ones

--- app/_lib/stripeLinks.ts
+++ app/_lib/stripeLinks.ts
@@ -1,7 +1,14 @@
 // app/_lib/stripeLinks.ts
+// export const STRIPE_LINKS = {
+//   adhesion: "https://buy.stripe.com/test_28E14ogeJgf73YT5wF3VC05",
+//   donation: "https://buy.stripe.com/test_aFaaEYbYtd2Vanh1gp3VC02",
+//   eventAdult: "https://buy.stripe.com/test_8x26oI5A5gf72UP9MV3VC06",
+//   eventChild: "https://buy.stripe.com/test_cNi14obYt5At6710cl3VC04",
+// } as const;
+
 export const STRIPE_LINKS = {
-  adhesion: "https://buy.stripe.com/test_28E14ogeJgf73YT5wF3VC05",
-  donation: "https://buy.stripe.com/test_aFaaEYbYtd2Vanh1gp3VC02",
-  eventAdult: "https://buy.stripe.com/test_8x26oI5A5gf72UP9MV3VC06",
-  eventChild: "https://buy.stripe.com/test_cNi14obYt5At6710cl3VC04",
-} as const;
+  adhesion: "https://buy.stripe.com/3cI6oI4xZ3d12Kj7vjf7i03",
+  donation: "https://buy.stripe.com/dRm9AUc0rcNBfx5eXLf7i04",
+  eventAdult: "https://buy.stripe.com/4gMeVe5C37th98HbLzf7i01",
+  eventChild: "https://buy.stripe.com/bJe28saWn5l9doX4j7f7i02",
+};
